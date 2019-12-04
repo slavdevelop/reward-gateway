3c211a8703375a6c6c52bd913fd01faea931962d
Slavi Shtipkov <development@shtipkov.com>
2019-12-04 16:03:37 +0200

    Created get request and successfully received the employee list

--- ui/src/components/App.js
+++ ui/src/components/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import axios from 'axios';
+
+const baseUrl = 'http://hiring.rewardgateway.net';
 
 const App = () => (
   <div>
     <h1>React Application</h1>
+    <button
+      onClick={() =>
+        axios
+          .get(`${baseUrl}/list`, {
+            auth: {
+              username: 'hard',
+              password: 'hard'
+            }
+          })
+          .then(response => {
+            console.log(response.data);
+          })
+      }
+    >
+      Test request
+    </button>
   </div>
 );
 
